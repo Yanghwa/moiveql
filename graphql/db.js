be74d46946de4f065e155dc086da5edaed20ddf1
Yanghwa <kellerj87@hotmail.com>
2018-10-16 01:23:29 -0400

    temp db content change

--- graphql/db.js
+++ graphql/db.js
@@ -1,31 +1,49 @@
-export const people = [
+let movies = [
     {
-        id: "0",
-        name: "Junghwan",
-        age: 30,
-        gender: "Male"
+        id: 0,
+        name: "Life is beutiful",
+        score: 10
     },
     {
-        id: "1",
-        name: "Te1",
-        age: 11,
-        gender: "Male"
+        id: 1,
+        name: "E.T.",
+        score: 9
     },
     {
-        id: "2",
-        name: "Ang",
-        age: 50,
-        gender: "Female"
+        id: 2,
+        name: "Yongari",
+        score: 2
     },
     {
-        id: "3",
-        name: "Hnv",
-        age: 28,
-        gender: "Male"
+        id: 3,
+        name: "D-war",
+        score: 3
     }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id  => {
-    const filteredPeople = people.filter(person => String(id) === person.id);
-    return filteredPeople[0];
+    const filteredMovie= movies.filter(movie => id === movie.id);
+    return filteredMovie[0];
 };
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if(movies.length > cleanedMovies.length) {
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
+}
